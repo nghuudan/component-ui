b4e390894267e733371275ab93acd1db7d61659f
Dan Nguyen <nghuudan@gmail.com>
2018-03-18 18:45:28 -0400

    Move files into components directory

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Button, Grid, Flex } from './components';
+import './app.scss';
+
+const App = () => (
+  <Grid className="app-container" container>
+    <Grid xs={12}>
+      <h1 className="app-title">Component UI &mdash; Simple components for React</h1>
+      <h2 className="app-heading">Button</h2>
+      <Flex>
+        <Button primary>primary</Button>
+        <Button warning>warning</Button>
+        <Button>default</Button>
+        <Button disabled>disabled</Button>
+      </Flex>
+    </Grid>
+  </Grid>
+);
+
+render(<App />, global.document.getElementById('app'));
