81bf92296c83878a72e6068805aba9160c8f78bc
Dan Nguyen <nghuudan@gmail.com>
2018-03-24 19:57:12 -0400

    Delete old js/jsx files

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export * from './button';
-export * from './flex';
-export * from './grid';
-export * from './spinner';
-export * from './utils';
--- src/components/utils/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Each } from './each';
-export { default as When } from './when';
